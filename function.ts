0cb505b239517d769848ec980f781d0a03e33274
junaid-sajjad-2298 <129473042+junaid-sajjad-2298@users.noreply.github.com>
2023-05-05 16:48:11 +0500

    Add files via upload

---
+++ function.ts
@@ -0,0 +1,13 @@
+// Concrete function example
+function multiply(a: number, b: number): number {
+    return a * b;
+  }
+  console.log(multiply(3, 4)); // Output: 12
+  
+  // Arrow function example
+  const divide = (a: number, b: number): number => {
+    return a / b;
+  };
+  console.log(divide(10, 5)); // Output: 2
+
+  

aa52221287139a4fad997b1320939f7ead2f0695
junaid-sajjad-2298 <129473042+junaid-sajjad-2298@users.noreply.github.com>
2023-05-05 16:20:35 +0500

    Add files via upload

---
+++ aroperator.ts
@@ -0,0 +1,23 @@
+console.log("i am in love with me @ junaid_sajjad");
+
+let num1= 10;
+let num2= 5;
+
+let result= num1 + num2; //Addition operator +
+console.log("Addition operator +:" +result);
+
+ result= num1 - num2; // Subtraction  operator -
+console.log("Subtraction  operator -:"+result);
+
+ result= num1 * num2; //Multiplication  operator *
+console.log("Multiplication  operator *:"+result);
+
+ result= num1 / num2; //Division  operator /
+console.log(" Division  operator /:"+result);
+
+ result= num1 % num2; //Remainder/Modulus   operator %
+console.log("Remainder/Modulus   operator %:"+result);
+
+ result= num1 ** num2; //Exponent operator **
+console.log(result);
+

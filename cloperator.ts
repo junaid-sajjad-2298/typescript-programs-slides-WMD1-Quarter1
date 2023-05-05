50a1187ccf9077553de1f325dd7d0bf8143e1eca
junaid-sajjad-2298 <129473042+junaid-sajjad-2298@users.noreply.github.com>
2023-05-05 16:22:45 +0500

    Add files via upload

---
+++ cloperator.ts
@@ -0,0 +1,33 @@
+// comparison operator examples
+
+let n=5
+console.log(n==5);
+console.log(n===5);
+console.log(n!=5);
+console.log(n>5);
+console.log(n=5);
+console.log(n<5);
+console.log(n>=5);
+console.log(n<=5);
+
+console.log("\n");
+
+//Logical Operator examples
+n=5
+ console.log(n>=5 && n<10);
+ console.log(n<=5 && n<10);
+ console.log(n>=5 || n<10);
+ console.log(n>5 || n<10);
+ console.log(!(n<10));
+ console.log(!(n>10));
+ console.log(!(n=5));
+
+ // that alll for now
+
+ 
+ 
+ 
+
+ 
+ 
+
